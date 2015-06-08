d2e81ca578a5755a2989b85b80a67f18ce00b037
Shawn148 <Shawn148@users.noreply.github.com>
2015-06-08 15:18:50 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -34,5 +34,5 @@ legend("topright", lty=1, col=c("black","red","blue"), cex=0.95,
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 # Save the second plot to a png file
-dev.copy(png, file="plot2.png", width=480, height=480)
+dev.copy(png, file="plot3.png", width=480, height=480)
 dev.off()
